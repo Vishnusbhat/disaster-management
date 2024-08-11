eb5b52ca6b03ab4ea45a55175dc75cd9f65b116e
Vishnusbhat <vishnusbhat3@gmail.com>
2024-08-11 09:02:55 +0530

    trying to host on netlify

--- disaster-management/src/App.jsx
+++ disaster-management/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import DisasterDetails from './pages/DisasterDetails';
